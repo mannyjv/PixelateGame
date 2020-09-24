d53cff3e9a5ee6cf805ffe0e855ecf5d12e00318
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2020-09-24 12:27:49 -0400

    table and basic color

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // Your code here
+
+const tableDom = document.getElementsByTagName("table")[0]
+
+function makeRow() {
+  const newRow = document.createElement("tr")
+
+  for (let i = 0; i < 20; i++) {
+    const newCell = document.createElement("td")
+    newRow.appendChild(newCell);
+  }
+
+  tableDom.appendChild(newRow);
+}
+
+const addRowButton = document.getElementById("add-row")
+
+addRowButton.addEventListener("click", makeRow)
+
+function colorize(event) {
+  if (event.target.className.length) {
+    event.target.className = "";
+  }
+  else {
+    event.target.className = "red";
+  }
+  //console.log("clicked")
+}
+
+tableDom.addEventListener("click", colorize)
+
