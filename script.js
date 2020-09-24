71d095e39f9ee5d7d4c0b40cdd68150055895284
Alexander Leon <1995.Alexander.Leon@gmail.com>
2020-09-24 10:00:17 -0700

    mostly working paint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Your code here
 
 const tableDom = document.getElementsByTagName("table")[0]
-
+let color = "red";
 function makeRow() {
   const newRow = document.createElement("tr")
 
@@ -18,14 +18,22 @@ const addRowButton = document.getElementById("add-row")
 addRowButton.addEventListener("click", makeRow)
 
 function colorize(event) {
-  if (event.target.className.length) {
+  if (event.target.tagName === 'TD'){
+  if ((event.target.className.length) && (event.target.className === color)) {
     event.target.className = "";
   }
   else {
-    event.target.className = "red";
-  }
-  //console.log("clicked")
+    event.target.className = color;
+  }}
+}
+tableDom.addEventListener("click", colorize)
+
+const select = document.getElementsByTagName('select')[0];
+
+select.addEventListener("change", changeColor)
+
+function changeColor(event){
+color = event.target.value;
 }
 
-tableDom.addEventListener("click", colorize)
 
