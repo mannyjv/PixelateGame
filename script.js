7ad910013071624a81e0ba93c6c2d265d09fc3ce
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2022-04-01 13:57:19 +0200

    feat: dragging to paint
    add code to allow user to click down on mouse and drag in order to paint over numerous cells. new event listeners added in order to listen for mousedown/mouseup events

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// Your code here
+const table = document.getElementsByTagName('table')[0]; //grab parent table element, must be indexed into since an this document method for finding elements returns an html collection
+const addRowButton = document.getElementById('add-row'); //grabbing the add row button so we can attach an event listener
+const select = document.getElementsByTagName('select')[0]; //grabbing select parent element that hosts the inner children option children elements for choosing each color
 
-const tableDom = document.getElementsByTagName('table')[0]; //grab parent table element, must be indexed into since an this document method for finding elements returns an html collection
 let chosenColor = 'red'; //default red as chosen color on load
+
+select.addEventListener('change', changeColor); //event object automatically gets passed as argument to the changeColor event handler function
+addRowButton.addEventListener('click', makeRow); //click event listener on addRowButton will invoke the makeRow func each time
+table.addEventListener('click', colorize); //lick is fired after a full click action occurs;the mouse button is pressed and released while the pointer remains inside the same element.
+table.addEventListener('mousedown', addMouseOver); //user clicks down on mouse and drags/hovers to other elements while click is still down- doesnt invoke colorize itself, instead invokes addMouseOver event which adds a listener to table for mouseover which then invokes colorize func
+table.addEventListener('mouseup', removeMouseOver); //fired at an Element when a button on a pointing device (such as a mouse or trackpad) is released while the pointer is located inside it.- handler is removeMouseOver which removes the mouseover listen from the table element
 
 function makeRow() {
   //func to create a new row will trigger everytime user clicks "add a row"
@@ -13,12 +20,8 @@ function makeRow() {
     newRow.appendChild(newCell); //to be inserted at the end of a parent node
   }
 
-  tableDom.appendChild(newRow); //append new finished row to be last row of table
+  table.appendChild(newRow); //append new finished row to be last row of table
 }
-
-const addRowButton = document.getElementById('add-row'); //grabbing the add row button so we can attach an event listener
-
-addRowButton.addEventListener('click', makeRow); //click event listener on addRowButton will invoke the makeRow func each time
 
 function colorize(event) {
   //func will trigger each time someone clicks on a cell in a row in the table
@@ -36,13 +39,19 @@ function colorize(event) {
     }
   }
 }
-tableDom.addEventListener('click', colorize); //add event listener on parent table element, will trigger colorize fun each time
-
-const select = document.getElementsByTagName('select')[0]; //grabbing select parent element that hosts the inner children option children elements for choosing each color
-
-select.addEventListener('change', changeColor); //event object automatically gets passed as argument to the changeColor event handler function
 
 function changeColor(event) {
   //func to change the reassign the color variable (line 4) after user has selected a new color to use
   chosenColor = event.target.value;
+}
+
+function addMouseOver() {
+  //triggered when user clicks down and moves pointer to other elements while click is still down
+  //table should now listen for the mouseover event that is fired at an Element when a pointing device (such as a mouse or trackpad) is used to move the cursor onto the element or one of its child elements. every time this event triggered we will invoke colorize to color each element the mouse is hovering over.
+  table.addEventListener('mouseover', colorize);
+}
+
+function removeMouseOver() {
+  //simply removes the mouseover event so that the coloring stops
+  table.removeEventListener('mouseover', colorize);
 }
