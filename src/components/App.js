fb9c290261c749912b7ee9595a23ee2f2f9f0f32
Emmanuel Vazquez <emmanuel.vazquez06@gmail.com>
2022-05-16 23:05:42 +0200

    feat: grid output
    adds code to export store, action creator addRow, handler func for onclick on the add row buttonso that user can add new rows, now starts with completely blank grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-
+import store, { addRow } from '../store.js';
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState(); //an initial state when component mounts
+    //use grid data in the render method- each row = a <tr> | each cell in the row = a <td>
+
+    this.handleAddRowClick = this.handleAddRowClick.bind(this);
+  }
+  //after component is mounted- subscribe to further updates
+  componentDidMount() {
+    //store.subscribe returns a function that will destroy the subscription (ie. unsubscribe).
+    //so now this class instance will have a method 'unsuscribe' which actually is assigned that returned function to destroy the subscription.
+    this.unsubscribe = store.subscribe(() => this.setState(store.getState())); //callback function will execute on every store update, resets state to keep it updated
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  handleAddRowClick() {
+    store.dispatch(addRow());
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div id="pixelate">
         <h1>Pixelate</h1>
-        <div>
-          <button id="add-row">Add a row</button>
+        <div id="buttons-container">
+          <button id="add-row" onClick={this.handleAddRowClick}>
+            Add a row
+          </button>
           <select>
             <option value="red">Red</option>
             <option value="orange">Orange</option>
@@ -20,7 +45,15 @@ export default class App extends React.Component {
             <option value="brown">Brown</option>
           </select>
         </div>
-        <table></table>
+        <table>
+          {this.state.grid.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((color, cellIndex) => (
+                <td key={cellIndex} className={color}></td>
+              ))}
+            </tr>
+          ))}
+        </table>
       </div>
     );
   }
--- src/store.js
+++ src/store.js
@@ -1,15 +1,25 @@
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import loggerMiddleware from 'redux-logger'; //prints out useful
-// information about everything that happens in your store
+import loggerMiddleware from 'redux-logger'; //prints out useful information about everything that happens in your store
 
-const initialState = {};
+// implement the state of the display as an empty array, Each pixel will be a string representing the color for that pixel. Blank pixels can be represented by an empty string: ''
+//will represent 1 row of 20 cells- the first row- default
+const NUM_COLUMNS = 20; //essentially each new row will be this [Array(20).fill('')], each element(row) is an array with 20 elements in it of "" empty strings at first before colors occurs
+const initialState = {
+  grid: [],
+};
 
 // ACTION TYPES
+const ADD_ROW = 'ADD_ROW';
 
 // ACTION CREATORS
+export const addRow = () => ({ type: ADD_ROW });
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_ROW:
+      const newRow = Array(NUM_COLUMNS).fill(''); //fill new array, with 20 elements ("")
+      //spread operator returns a *new* object, not a mutated one
+      return { ...state, grid: [...state.grid, newRow] };
     default:
       return state;
   }
